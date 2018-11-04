46380fe21d548804137dbe035dbb0e3b83876d15
Christopher McCormack <mccormack.christopher@gmail.com>
2018-11-04 15:23:42 -0700

    Added thread view handler

--- routes/apiview.js
+++ routes/apiview.js
@@ -1,31 +1,42 @@
 const router = require("express").Router()
 const ThreadHandler = require('../controllers/threadHandler')
+const ReplyHandler = require('../controllers/replyHandler')
 
 
 module.exports = () => {
 
   const threadHandler = new ThreadHandler()
+  const replyHandler = new ReplyHandler()
 
   ///////////////////////////////////////////////////////////
   // View Board Threads
   ///////////////////////////////////////////////////////////
-  router.route('/:board')
+  router.route('/:board/')
     .get((req, res, next) => {
       const { board } = req.params
 
       threadHandler.getRecentThreads(
-        { params: {board} },
+        { params: { board } },
         { json: threads => {
-          res.render('board', {board, threads})
-        }})
-      
+          res.render('board', { board, threads })
+        }}
+      )
+
     })
 
-  router.route('/:board/:thread_id')
+  router.route('/:board/:thread_id/')
     .get((req, res, next) => {
+      const { board, thread_id } = req.params
 
-      res.render('thread', { success: true })
-
+      replyHandler.getThreadWithReplies(
+        { 
+          params: { board },
+          query: { thread_id }
+        },
+        { json: thread => {
+          res.render('thread', { board, thread })
+        }}
+      )
     })
 
   return router
